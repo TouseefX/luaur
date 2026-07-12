009f477e87bd452c7edbc119ee5629f4372b8e3b
TouseefX <131637263+TouseefX@users.noreply.github.com>
2026-07-12 20:36:52 +0400

    Fix the bug formatting
    We fixed a bug then we fixed it again???

--- crates/luaur-conformance/src/functions/userdata_api_inline_char_dtor.rs
+++ crates/luaur-conformance/src/functions/userdata_api_inline_char_dtor.rs
@@ -4,5 +4,8 @@ use core::sync::atomic::Ordering;
 use crate::functions::userdata_api_dtor_hits::USERDATA_API_DTOR_HITS;
 
 pub unsafe extern "C" fn userdata_api_inline_char_dtor(data: *mut c_void) {
-    USERDATA_API_DTOR_HITS.fetch_add(*(data as *const core::ffi::c_char) as u8 as i32, Ordering::SeqCst);
+    USERDATA_API_DTOR_HITS.fetch_add(
+        *(data as *const core::ffi::c_char) as u8 as i32,
+        Ordering::SeqCst,
+    );
 }
